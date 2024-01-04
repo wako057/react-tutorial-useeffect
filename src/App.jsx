43fe4c93a9dcfc6e2d9af4436848dd4e42ec62fc
Gregory Gerard <gregory@flatchr.io>
2024-01-04 15:24:03 +0100

    bug: create infinite loop

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [ availablePlaces, setAvailablePlaces ] = useState([])
 
   navigator.geolocation.getCurrentPosition((position) => {
     console.log('position: ', position);
-    sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+    const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
   });
 
   function handleStartRemovePlace(id) {
